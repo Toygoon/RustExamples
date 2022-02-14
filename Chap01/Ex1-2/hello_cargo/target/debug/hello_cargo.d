a87518d097bf32b53fc086a62383dc96804a6420
Toygoon <toygoon@gmail.com>
2022-02-14 19:25:43 +0900

    Ex1-2, Start a project with cargo

---
+++ Chap01/Ex1-2/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/toygoon/Code/RustExamples/Chap01/Ex1-2/hello_cargo/target/debug/hello_cargo: /home/toygoon/Code/RustExamples/Chap01/Ex1-2/hello_cargo/src/main.rs
---
+++ Chap01/Ex1-2/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/toygoon/Code/RustExamples/Chap01/Ex1-2/hello_cargo/target/release/hello_cargo: /home/toygoon/Code/RustExamples/Chap01/Ex1-2/hello_cargo/src/main.rs
