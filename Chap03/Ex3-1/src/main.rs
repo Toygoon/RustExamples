a7bb720c97b17a988839e96bd1abfb5ee4b2e73b
Toygoon <toygoon@gmail.com>
2022-02-24 15:46:46 +0900

    Ex3-2-2, Array definings

--- Chap03/Ex3-1/src/main.rs
+++ Chap03/Ex3-1/src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March",
+                    "April", "May", "June",
+                    "July", "August", "September",
+                    "October", "November", "December"];
+    let threes = [3; 5];
 
-    let five_hundred = x.0;
-
-    let six_point_four = x.1;
-
-    let one = x.2;
-
-    println!("five_hundred = {}", five_hundred);
-    println!("six_point_four = {}", six_point_four);
-    println!("one = {}", one);
+    let first = a[0];
+    let second = a[1];
 }
