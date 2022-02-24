4424175e0257d386ddb26e5a7a3ed9a3b3627166
Toygoon <toygoon@gmail.com>
2022-02-24 15:10:38 +0900

    Ex2-2, Rand numbers

--- Chap02/Ex2-1/guessing_game/src/main.rs
+++ Chap02/Ex2-1/guessing_game/src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Let's guess a number.");
     println!("Input a number.");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("System generated value : {}", secret_number);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Couldn't read a number.");
 
