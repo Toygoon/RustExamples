04ae7a224aa766cd0f68b8a54061123033947257
Toygoon <toygoon@gmail.com>
2022-02-24 15:28:33 +0900

    Ex2-3, Loop input

--- Chap02/Ex2-1/guessing_game/src/main.rs
+++ Chap02/Ex2-1/guessing_game/src/main.rs
@@ -1,14 +1,28 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Let's guess a number.");
-    println!("Input a number.");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("System generated value : {}", secret_number);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Couldn't read a number.");
+    
+    println!("Let's guess a number.");
 
-    println!("Input value : {}", guess);
+    loop {
+        println!("Input a number.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Couldn't read a number.");
+
+        let guess: i32 = guess.trim().parse().expect("Invalid input.");
+        println!("Input value : {}", guess);
+            
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less number"),
+            Ordering::Greater => println!("Greater number"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;}
+        }
+    }
 }
