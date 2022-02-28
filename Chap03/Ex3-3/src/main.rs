6bd020d85fe5e70a3bdfe07dd3dd6bd22602a185
Toygoon <toygoon@gmail.com>
2022-02-28 13:30:29 +0900

    Ex3-3-1, Function parameters

--- Chap03/Ex3-3/src/main.rs
+++ Chap03/Ex3-3/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     another_function();
 }
 
-fn another_function() {
-    println!("Another function!");
+fn another_function(x: i32, y: i32) {
+    println!("x의 값 : {}", x);
+    println!("y의 값 : {}", y);
 }
