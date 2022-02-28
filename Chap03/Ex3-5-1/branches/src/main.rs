22d6b6c1b523bbb18f57ceedf010e3678f7091a5
Toygoon <toygoon@gmail.com>
2022-02-28 14:55:16 +0900

    Ex3-5-1, if expressions

--- Chap03/Ex3-5-1/branches/src/main.rs
+++ Chap03/Ex3-5-1/branches/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number < 5 {
-        println!("Number is in the condition.");
+    if number % 4 == 0 {
+        println!("The number is divided into 4.");
+    } else if number % 3 == 0 {
+        println!("The number is divided into 3");
+    } else if number % 2 == 0 {
+        println!("The number is divided into 2");
     } else {
-        println!("Number is not in the condition.");
+        println!("The number doesn't be divided into 2 or 3 or 4.");
     }
 }
