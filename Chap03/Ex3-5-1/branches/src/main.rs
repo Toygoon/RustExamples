36637795d6d193b0a41c93bfc78ecbcfcac18f76
Toygoon <toygoon@gmail.com>
2022-02-28 14:52:03 +0900

    Ex3-5-1, First condition

---
+++ Chap03/Ex3-5-1/branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("Number is in the condition.");
+    } else {
+        println!("Number is not in the condition.");
+    }
+}
