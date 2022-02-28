f25c8a23b6b062d3f1dd4c1a47bc3ffa41ef1698
Toygoon <toygoon@gmail.com>
2022-02-28 14:45:12 +0900

    Ex3-3-3, Returning values

--- Chap03/Ex3-3-3/src/main.rs
+++ Chap03/Ex3-3-3/src/main.rs
@@ -1,9 +1,8 @@
-fn five() -> i32 {
-    5
+fn main() {
+    let x = plus_one(5);
+    println!("x = {}", x);
 }
 
-fn main() {
-    let x = five();
-
-    println!("x = {}", x);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
