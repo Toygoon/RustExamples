e377a01ed1975cb6655225b3462404ac18031584
Toygoon <toygoon@gmail.com>
2022-03-01 15:10:40 +0900

    No more tuples, Reference expression

--- Chap04/Ex4-5/src/main.rs
+++ Chap04/Ex4-5/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     let s1 = String::from("Hello");
 
     let (s2, len) = calculate_length(s1);
@@ -10,4 +10,16 @@ fn calculate_length(s: String) -> (String, usize) {
     let length = s.len();
 
     (s, length)
+}
+*/
+
+fn main() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+
+    println!("Length of '{}' is {}", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
